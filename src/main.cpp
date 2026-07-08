bbd18ad9b1e4623e6108bac9958c702765909a97
Aledve <jakile971012@gmail.com>
2026-07-08 15:21:12 -0500

    Update: General changes

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,29 @@ const int NUM_BARRERAS = 18;
 
 // Tópicos de MQTT para cada barrera (IN y OUT) con sus respectivos UUIDs
 const char* topicos_barreras[NUM_BARRERAS] = {
-  "upark/acceso/puerta/b62905f1-df85-4353-abed-9e22aeb7f127",  // 0: Puerta 1 - IN
-  "upark/acceso/puerta/638bf422-99d0-4cc4-bdb3-50d12b05159a",  // 1: Puerta 1 - OUT
-  "upark/acceso/puerta/7763aec5-3fe8-484a-ac57-993d89d51ce7",  // 2: Puerta 2 - IN
-  "upark/acceso/puerta/e2d0bdfb-9ce9-4574-9918-b950c5c23d43",  // 3: Puerta 2 - OUT
-  "upark/acceso/puerta/ea0fcf4c-710f-4a5a-bcef-c37a3838ac55",  // 4: Puerta 3 - IN
-  "upark/acceso/puerta/c2e34a47-22fc-4570-a6c4-1347316ca31e",  // 5: Puerta 3 - OUT
-  "upark/acceso/puerta/3717f1c2-6283-4361-a9f4-84d3ab70437b",  // 6: Puerta 4 - IN
-  "upark/acceso/puerta/8f824a1f-3a41-4952-a4de-56d4de441f2c",  // 7: Puerta 4 - OUT
-  "upark/acceso/puerta/9d71ebf1-d7bf-4ba2-8417-593e90afcf3a",  // 8: Puerta 5 - IN
-  "upark/acceso/puerta/537bbea7-f8ba-4583-a746-59098db8bbb6",  // 9: Puerta 5 - OUT
-  "upark/acceso/puerta/1f974843-4f97-4e13-8034-7cd3103767f4",  // 10: Puerta 6 - IN
-  "upark/acceso/puerta/c6ed8692-38b7-4f6d-9246-7fed80633359",  // 11: Puerta 6 - OUT
-  "upark/acceso/puerta/a574c6b6-e57b-4a20-92f5-790dc0a5fec1",  // 12: Puerta 7 - IN
-  "upark/acceso/puerta/eee7e688-9963-4d7a-b6ae-5342f79b598b",  // 13: Puerta 7 - OUT
-  "upark/acceso/puerta/18b5f8c6-2337-4ba5-800a-25a8777b31be",  // 14: Puerta 8 - IN
-  "upark/acceso/puerta/f2c61ae5-35c1-4c53-ac73-f843d5dd6306",  // 15: Puerta 8 - OUT
-  "upark/acceso/puerta/f7eff2fd-b383-4adf-81fe-18f42ba4e10e",  // 16: Puerta 9 - IN
-  "upark/acceso/puerta/73a44545-37fe-4579-bf02-f2804de4a097"   // 17: Puerta 9 - OUT
+  "upark/acceso/puerta/b62905f1-df85-4353-abed-9e22aeb7f127",  // Puerta 1 - IN
+  "upark/acceso/puerta/638bf422-99d0-4cc4-bdb3-50d12b05159a",  // Puerta 1 - OUT
+  "upark/acceso/puerta/7763aec5-3fe8-484a-ac57-993d89d51ce7",  // Puerta 2 - IN
+  "upark/acceso/puerta/e2d0bdfb-9ce9-4574-9918-b950c5c23d43",  // Puerta 2 - OUT
+  "upark/acceso/puerta/ea0fcf4c-710f-4a5a-bcef-c37a3838ac55",  // Puerta 3 - IN
+  "upark/acceso/puerta/c2e34a47-22fc-4570-a6c4-1347316ca31e",  // Puerta 3 - OUT
+  "upark/acceso/puerta/3717f1c2-6283-4361-a9f4-84d3ab70437b",  // Puerta 4 - IN
+  "upark/acceso/puerta/8f824a1f-3a41-4952-a4de-56d4de441f2c",  // Puerta 4 - OUT
+  "upark/acceso/puerta/9d71ebf1-d7bf-4ba2-8417-593e90afcf3a",  // Puerta 5 - IN
+  "upark/acceso/puerta/537bbea7-f8ba-4583-a746-59098db8bbb6",  // Puerta 5 - OUT
+  "upark/acceso/puerta/1f974843-4f97-4e13-8034-7cd3103767f4",  // Puerta 6 - IN
+  "upark/acceso/puerta/c6ed8692-38b7-4f6d-9246-7fed80633359",  // Puerta 6 - OUT
+  "upark/acceso/puerta/a574c6b6-e57b-4a20-92f5-790dc0a5fec1",  // Puerta 7 - IN
+  "upark/acceso/puerta/eee7e688-9963-4d7a-b6ae-5342f79b598b",  // Puerta 7 - OUT
+  "upark/acceso/puerta/18b5f8c6-2337-4ba5-800a-25a8777b31be",  // Puerta 8 - IN
+  "upark/acceso/puerta/f2c61ae5-35c1-4c53-ac73-f843d5dd6306",  // Puerta 8 - OUT
+  "upark/acceso/puerta/f7eff2fd-b383-4adf-81fe-18f42ba4e10e",  // Puerta 9 - IN
+  "upark/acceso/puerta/73a44545-37fe-4579-bf02-f2804de4a097"   // Puerta 9 - OUT
 };
 
-// Pines de los LEDs
 const int PIN_LED_VERDE = 2;
 const int PIN_LED_ROJO = 0; 
 
-// Los 18 pines asignados a los servomotores (Coinciden con diagram.json)
-// Orden: P1_IN, P1_OUT, P2_IN, P2_OUT, P3_IN, P3_OUT...
 const int pinesServo[NUM_BARRERAS] = {
   4, 19,   // Puerta 1
   5, 21,   // Puerta 2
@@ -84,7 +81,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(": ");
   Serial.println(mensaje);
 
-  // Lógica de control de la barrera (Lógica original de Daniel)
+  // Lógica de control de la barrera
   if (mensaje == "abrir") {
     Serial.println(">>> ACCESO PERMITIDO: Abriendo barrera...");
     
