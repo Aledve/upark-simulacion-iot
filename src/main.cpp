cc21e5b834f1ce40dd30c28ccf436a5929f04d99
Aledve <jakile971012@gmail.com>
2026-07-08 15:16:47 -0500

    Update: Addition of UUIDs for each barrier at each entrance.

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,50 @@ const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 const char* mqtt_server = "broker.hivemq.com";
 
-const int NUM_PUERTAS = 9;
+const int NUM_BARRERAS = 18;
 
-// Tópicos de MQTT
-const char* topicos_puertas[NUM_PUERTAS] = {
-  "upark/acceso/puerta/c50e4adb-e5bf-47e7-b297-7be877789b44", 
-  "upark/acceso/puerta/43481483-fc4d-48b0-b7e6-e014330c67b1", 
-  "upark/acceso/puerta/21410856-c0ce-40ec-a4c9-ec173fb9ba0c",
-  "upark/acceso/puerta/1ef56d32-7cc0-4ebd-abd0-1abbe349c1cf", 
-  "upark/acceso/puerta/0e0e124e-78e2-4106-a887-2ff0aa844729", 
-  "upark/acceso/puerta/f7bf1f2e-cba3-4487-82d4-9693eb5e7bf0",
-  "upark/acceso/puerta/5142ed92-9aa3-4c48-b5af-7c02271e8753", 
-  "upark/acceso/puerta/468fb2d0-e369-4b5b-be99-0fb744efdae3", 
-  "upark/acceso/puerta/91564372-412a-4bce-b142-c43cd6e62bcb"
+// Tópicos de MQTT para cada barrera (IN y OUT) con sus respectivos UUIDs
+const char* topicos_barreras[NUM_BARRERAS] = {
+  "upark/acceso/puerta/b62905f1-df85-4353-abed-9e22aeb7f127",  // 0: Puerta 1 - IN
+  "upark/acceso/puerta/638bf422-99d0-4cc4-bdb3-50d12b05159a",  // 1: Puerta 1 - OUT
+  "upark/acceso/puerta/7763aec5-3fe8-484a-ac57-993d89d51ce7",  // 2: Puerta 2 - IN
+  "upark/acceso/puerta/e2d0bdfb-9ce9-4574-9918-b950c5c23d43",  // 3: Puerta 2 - OUT
+  "upark/acceso/puerta/ea0fcf4c-710f-4a5a-bcef-c37a3838ac55",  // 4: Puerta 3 - IN
+  "upark/acceso/puerta/c2e34a47-22fc-4570-a6c4-1347316ca31e",  // 5: Puerta 3 - OUT
+  "upark/acceso/puerta/3717f1c2-6283-4361-a9f4-84d3ab70437b",  // 6: Puerta 4 - IN
+  "upark/acceso/puerta/8f824a1f-3a41-4952-a4de-56d4de441f2c",  // 7: Puerta 4 - OUT
+  "upark/acceso/puerta/9d71ebf1-d7bf-4ba2-8417-593e90afcf3a",  // 8: Puerta 5 - IN
+  "upark/acceso/puerta/537bbea7-f8ba-4583-a746-59098db8bbb6",  // 9: Puerta 5 - OUT
+  "upark/acceso/puerta/1f974843-4f97-4e13-8034-7cd3103767f4",  // 10: Puerta 6 - IN
+  "upark/acceso/puerta/c6ed8692-38b7-4f6d-9246-7fed80633359",  // 11: Puerta 6 - OUT
+  "upark/acceso/puerta/a574c6b6-e57b-4a20-92f5-790dc0a5fec1",  // 12: Puerta 7 - IN
+  "upark/acceso/puerta/eee7e688-9963-4d7a-b6ae-5342f79b598b",  // 13: Puerta 7 - OUT
+  "upark/acceso/puerta/18b5f8c6-2337-4ba5-800a-25a8777b31be",  // 14: Puerta 8 - IN
+  "upark/acceso/puerta/f2c61ae5-35c1-4c53-ac73-f843d5dd6306",  // 15: Puerta 8 - OUT
+  "upark/acceso/puerta/f7eff2fd-b383-4adf-81fe-18f42ba4e10e",  // 16: Puerta 9 - IN
+  "upark/acceso/puerta/73a44545-37fe-4579-bf02-f2804de4a097"   // 17: Puerta 9 - OUT
 };
 
-// Pines
+// Pines de los LEDs
 const int PIN_LED_VERDE = 2;
-const int PIN_LED_ROJO = 4;
-const int pinesServo[NUM_PUERTAS] = {12, 13, 14, 15, 16, 17, 18, 19, 21}; 
+const int PIN_LED_ROJO = 0; 
 
-Servo barreras[NUM_PUERTAS];
+// Los 18 pines asignados a los servomotores (Coinciden con diagram.json)
+// Orden: P1_IN, P1_OUT, P2_IN, P2_OUT, P3_IN, P3_OUT...
+const int pinesServo[NUM_BARRERAS] = {
+  4, 19,   // Puerta 1
+  5, 21,   // Puerta 2
+  12, 22,  // Puerta 3
+  13, 23,  // Puerta 4
+  14, 25,  // Puerta 5
+  15, 26,  // Puerta 6
+  16, 27,  // Puerta 7
+  17, 32,  // Puerta 8
+  18, 33   // Puerta 9
+}; 
+
+Servo barreras[NUM_BARRERAS];
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -44,41 +67,57 @@ void callback(char* topic, byte* payload, unsigned int length) {
     mensaje += (char)payload[i];
   }
   
-  // Identificar qué puerta recibió el comando
-  int puertaIndex = -1;
-  for(int i = 0; i < NUM_PUERTAS; i++){
-    if(String(topic) == String(topicos_puertas[i])){
-      puertaIndex = i;
+  // Identificar qué barrera específica (índice 0-17) recibió el comando
+  int barreraIndex = -1;
+  for(int i = 0; i < NUM_BARRERAS; i++){
+    if(String(topic) == String(topicos_barreras[i])){
+      barreraIndex = i;
       break;
     }
   }
 
-  if(puertaIndex == -1) return;
+  // Si el UUID no coincide con ninguna barrera registrada, se ignora
+  if(barreraIndex == -1) return;
 
-  Serial.print(">>> Acción en PUERTA ");
-  Serial.print(puertaIndex + 1);
+  Serial.print(">>> Acción en BARRERA índice ");
+  Serial.print(barreraIndex);
   Serial.print(": ");
   Serial.println(mensaje);
 
+  // Lógica de control de la barrera (Lógica original de Daniel)
   if (mensaje == "abrir") {
+    Serial.println(">>> ACCESO PERMITIDO: Abriendo barrera...");
+    
     // ESTADO GENERAL: Abriendo (Verde ON, Rojo OFF)
     digitalWrite(PIN_LED_ROJO, LOW);
     digitalWrite(PIN_LED_VERDE, HIGH);
     
-    // Abrir barrera específica
-    barreras[puertaIndex].write(90); 
+    // Abrir SOLAMENTE la barrera que fue autorizada
+    barreras[barreraIndex].write(90); 
     
-    delay(5000); // Espera 5 segundos
+    // Espera 5 segundos a que pase el auto
+    delay(5000); 
     
-    // Cerrar barrera específica
-    barreras[puertaIndex].write(0);  
+    // Cerrar la barrera
+    barreras[barreraIndex].write(0);  
     
     // ESTADO GENERAL: Cerrado (Verde OFF, Rojo ON)
     digitalWrite(PIN_LED_VERDE, LOW);
     digitalWrite(PIN_LED_ROJO, HIGH);
     
-    Serial.println("Barrera cerrada.");
+    Serial.println(">>> Barrera cerrada.");
   } 
+  else if (mensaje == "denegado") {
+    Serial.println(">>> ACCESO DENEGADO");
+    
+    // Parpadeo de alerta en el LED rojo
+    for(int i = 0; i < 3; i++) {
+      digitalWrite(PIN_LED_ROJO, LOW);
+      delay(200);
+      digitalWrite(PIN_LED_ROJO, HIGH);
+      delay(200);
+    }
+  }
 }
 
 void reconnect() {
@@ -88,9 +127,9 @@ void reconnect() {
 
     if (client.connect(clientId.c_str())) {
       Serial.println("¡Conectado a HiveMQ!");
-      // Suscribirse a las 9 puertas
-      for(int i = 0; i < NUM_PUERTAS; i++){
-         client.subscribe(topicos_puertas[i]);
+      // Suscribirse a los 18 tópicos (IN y OUT) a la vez
+      for(int i = 0; i < NUM_BARRERAS; i++){
+         client.subscribe(topicos_barreras[i]);
       }
     } else {
       delay(5000);
@@ -109,8 +148,8 @@ void setup() {
   digitalWrite(PIN_LED_VERDE, LOW);
   digitalWrite(PIN_LED_ROJO, HIGH);
 
-  // Inicializar Servos
-  for(int i = 0; i < NUM_PUERTAS; i++){
+  // Inicializar y asignar pines a los 18 Servomotores
+  for(int i = 0; i < NUM_BARRERAS; i++){
     barreras[i].attach(pinesServo[i]);
     barreras[i].write(0);
   }
